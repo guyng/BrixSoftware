5d10f974a6746ce7782be80851fcaa10df5c54af
guyng@mail.mta.ac.il <guyng@mail.mta.ac.il>
2019-12-10 15:29:39 +0200

    menu

--- Program.cs
+++ Program.cs
@@ -7,26 +7,43 @@ namespace Solutions
 	{
 		static async Task Main(string[] args)
 		{
+			Console.WriteLine("Choose solution: (1 or 2)");
+			var input = Console.ReadLine();
+			if (input == "1")
+			{
+				await Solution1();
+			}
+			else if (input == "2")
+			{
+				Solution2();
+			}
+		}
 
+		public static async Task Solution1()
+		{
 			Solution1 sol1 = new Solution1();
 			await sol1.StartProccessing();
+		}
 
+		public static void Solution2()
+		{
+			Console.WriteLine("Please wait...");
+			Solution2 sol2 = new Solution2();
+			Console.WriteLine("Please input 5 letter string");
+			var input = Console.ReadLine();
+	//		var testInput = "oznbg"; //example
+			var matches = sol2.SearchForString(input);
 
-			//Solution2 sol2 = new Solution2();
-			//var input = Console.ReadLine();
-			//var testInput = "oznbg"; //example
-			//var matches = sol2.SearchForString(testInput);
-
-			//Console.WriteLine($"Test Input: {testInput}");
-			//Console.WriteLine($"Matches Count: {matches.Count}");
-			//if (matches.Count > 0)
-			//{
-			//	Console.WriteLine($"Matches:");
-			//	foreach (var match in matches)
-			//	{
-			//		Console.WriteLine(match);
-			//	}
-			//}
+			Console.WriteLine($"Input: {input}");
+			Console.WriteLine($"Matches Count: {matches.Count}");
+			if (matches.Count > 0)
+			{
+				Console.WriteLine($"Matches:");
+				foreach (var match in matches)
+				{
+					Console.WriteLine(match);
+				}
+			}
 		}
 	}
 }
